3855d366cffc6366a123ee28f1d9cc6848019979
RannerJP <RannerJP@gmail.com>
2021-05-10 21:15:49 -0700

    Made test graph simpler

--- src/testGraph.java
+++ src/testGraph.java
@@ -1,28 +1,10 @@
+import java.io.FileNotFoundException;
+
 public class testGraph{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
         Graph<Character> test = new Graph<>(9);
-        test.setLabel(0, 'a');
-        test.setLabel(1, 'b');
-        test.setLabel(2, 'c');
-        test.setLabel(3, 'd');
-        test.setLabel(4, 'e');
-        test.setLabel(5, 'f');
-        test.setLabel(6, 'g');
-        test.setLabel(7, 'h');
-        test.setLabel(8, 'i');
-        test.addEdge(0, 3);
-        test.addEdge(0, 4);
-        test.addEdge(0, 2);
-        test.addEdge(1, 4);
-        test.addEdge(2, 1);
-        test.addEdge(3, 6);
-        test.addEdge(4, 5);
-        test.addEdge(4, 7);
-        test.addEdge(5, 2);
-        test.addEdge(5, 7);
-        test.addEdge(6, 7);
-        test.addEdge(7, 8);
-        test.addEdge(8, 5);
+        test.autoSetLabels();
+        test.importGraph("TestGraph.csv");
         String result = test.getDepthFirstTraversal(0);
         System.out.println(result);
     }
